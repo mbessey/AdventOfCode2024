e321d66fd8acee503afb6d81eac9d0525eca8e95
Mark Bessey <mark_bessey@yahoo.com>
2024-12-02 21:31:09 -0800

    Parts One and Two done.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,49 +1,62 @@
+use regex::Regex;
 
 fn main() {
-    let values = file_as_vec2("src/test.txt");
-    pretty_print(&values);
-    println!("{}", row_solution(&values[0]));
     part1();
     part2();
 }
 
-fn row_solution(values: &Vec<i32>) -> String {
-    return "42".to_string();
-}
-
 fn part1() {
-
+    println!("PART ONE");
+    let text = file_contents("src/part1.txt");
+    let txt = text.as_str();
+    let re= Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+    let mut sum = 0;
+    for capture_val in re.captures_iter(txt) {
+        print!("{}, {} * {} = ", &capture_val[0], &capture_val[1], &capture_val[2]);
+        let l = capture_val[1].parse::<i32>().unwrap();
+        let r = capture_val[2].parse::<i32>().unwrap();
+        println!("{}", l * r);
+        sum += (l * r);
+    }
+    println!("==========================\n{}", sum);
 }
 
 fn part2() {
-
-}
-
-fn file_as_vec2(path: &str) -> Vec<Vec<i32>> {
-    let mut result:Vec<Vec<i32>> = Vec::new();
-    let contents = file_contents(path);
-    let rows = contents.lines();
-    for row in rows {
-        let mut row_vec:Vec<i32> = Vec::new();
-        let values = row.split_whitespace();
-        for value in values {
-            let v = value.parse::<i32>()
-            .expect("Failed to parse");
-            row_vec.push(v);
+    println!("PART TWO");
+    let text = file_contents("src/part1.txt");
+    let txt = text.as_str();
+    let re= Regex::new(r"mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\)").unwrap();
+    let mut sum = 0;
+    let mut active = true;
+    for capture_val in re.captures_iter(txt) {
+        let instruction = &capture_val[0];
+        match instruction {
+            "do()" => {
+                active = true;
+                println!("DO");
+            },
+            "don't()" => {
+                active = false;
+                println!("DON'T");
+            },                
+            _ => {
+                if active {
+                    print!("{}, {} * {} = ", &capture_val[0], &capture_val[1], &capture_val[2]);
+                    let l = capture_val[1].parse::<i32>().unwrap();
+                    let r = capture_val[2].parse::<i32>().unwrap();
+                    println!("{}", l * r);
+                    sum += (l * r);
+                } else {
+                    println!("skipped");
+                }
+            }
         }
-        result.push(row_vec);
     }
-    return result;
+    println!("==========================\n{}", sum);
 }
 
 fn file_contents(path: &str) -> String {
     use std::fs;
     return fs::read_to_string(path)
         .expect("should have read the file");
-}
-
-fn pretty_print(vec:&Vec<Vec<i32>>) {
-    for row in vec {
-        println!("{:?}, ", row);
-    }
 }
