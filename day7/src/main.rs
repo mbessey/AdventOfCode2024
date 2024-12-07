482e48963ddd9cdc2c4e70ef5d4c28a43bc2721d
Mark Bessey <mark_bessey@yahoo.com>
2024-12-06 22:31:14 -0800

    Part 2 passes!

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,31 +2,34 @@
 fn main() {
     sample();
     part1();
-    // part2();
+    part2();
 }
 
 fn sample() {
     println!("Sample Data");
     let equations = file_as_equations("src/sample.txt");
     // pretty_print(&equations);
-    solve(&equations);
+    solve(&equations, true);
 }
 
 fn part1() {
     println!("Part 1");
     let equations = file_as_equations("src/part1.txt");
     // pretty_print(&equations);
-    solve(&equations);
+    solve(&equations, false);
 }
 
 fn part2() {
     println!("Part 2");
+    let equations = file_as_equations("src/part1.txt");
+    // pretty_print(&equations);
+    solve(&equations, true);
 }
 
-fn solve(equations: &Vec<Equation>) {
+fn solve(equations: &Vec<Equation>, allow_concat:bool) {
     let mut total = 0;
     for eq in equations {
-        let ops = operations(&eq);
+        let ops = operations(&eq, allow_concat);
         if ops.len() > 0 {
             print!("{} = {}", eq.test_value, eq.operands[0]);
             for i in 0..ops.len() {
@@ -41,7 +44,7 @@ fn solve(equations: &Vec<Equation>) {
     println!("TOTAL: {}", total);    
 }
 
-fn operations(e: &Equation) -> Vec<char> {
+fn operations(e: &Equation, allow_concat:bool) -> Vec<char> {
     let test_value = e.test_value;
     let operands = &e.operands;
     let spaces = e.operands.len() - 1;
@@ -51,6 +54,13 @@ fn operations(e: &Equation) -> Vec<char> {
         } else if operands[0] * operands[1] == test_value{
             return vec!['*']
         } else {
+            if allow_concat {
+                let s = format!("{}{}", operands[0], operands[1]);
+                let appended = s.parse::<i64>().unwrap();
+                if appended == test_value {
+                    return vec!['|']
+                }
+            }
             return vec![]
         }
     } else {
@@ -62,7 +72,7 @@ fn operations(e: &Equation) -> Vec<char> {
             test_value: test_value,
             operands: ops,
         };
-        let mut sub_ops = operations(&e2);
+        let mut sub_ops = operations(&e2, allow_concat);
         if sub_ops.len() > 0 {
             let mut result = vec!['*'];
             result.append(&mut sub_ops);
@@ -76,12 +86,29 @@ fn operations(e: &Equation) -> Vec<char> {
             test_value: test_value,
             operands: ops
         };
-        let mut sub_ops = operations(&e2);
+        let mut sub_ops = operations(&e2, allow_concat);
         if sub_ops.len() > 0 {
             let mut result = vec!['+'];
             result.append(&mut sub_ops);
             return result
         }
+        // the concat operation...
+        if allow_concat {
+            let combined_ops = format!("{}{}", operands[0], operands[1]);
+            let new_op = combined_ops.parse::<i64>().unwrap();
+            let mut ops = vec![new_op];
+            ops.extend(operands[2..].iter());
+            let e2 = Equation {
+                test_value: test_value,
+                operands: ops
+            };
+            let mut sub_ops = operations(&e2, allow_concat);
+            if sub_ops.len() > 0 {
+                let mut result = vec!['|'];
+                result.append(&mut sub_ops);
+                return result
+            }
+       }
     }
     return vec![];
 }
