072fdabd42eb2155b1c42b065843221d697636a7
Mark Bessey <mark_bessey@yahoo.com>
2024-12-10 22:52:21 -0800

    Part 1 works, Part 2 is VERY slow!

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,10 +1,4 @@
-use std::{collections::HashMap};
-
-#[derive(Debug)]
-struct Coord {
-    row: i32,
-    col: i32
-}
+use std::{collections::HashMap, str::FromStr};
 
 fn main() {
     sample();
@@ -14,31 +8,65 @@ fn main() {
 
 fn sample() {
     println!("Sample: ");
-    let values = file_as_vec2("src/sample.txt");
-    //pretty_print(&values);
+    let mut stones = file_as_vec("src/sample.txt");
+    println!("{:?}", stones);
+    for _i in 0 ..6 {
+        stones = evolve_stones(stones);
+        println!("{:?}", stones);
+    }
+    let mut stones = file_as_vec("src/sample.txt");
+    for i in 0..25 {
+        stones = evolve_stones(stones);
+    }
+    println!("After 25 blinks, {} stones", stones.len());
 }
 
 fn part1() {
     println!("PART ONE: ");
-
+    let mut stones = file_as_vec("src/part1.txt");
+    println!("{:?}", stones);
+    for i in 0..25 {
+        stones = evolve_stones(stones);
+    }
+    println!("After 25 blinks, {} stones", stones.len());
 }
 
 fn part2() {
     println!("PART TWO: ");
-
+    let mut stones = file_as_vec("src/part1.txt");
+    println!("{:?}", stones);
+    for i in 0..75 {
+        println!("{}: {}", i, stones.len());
+        stones = evolve_stones(stones);
+    }
+    println!("After 75 blinks, {} stones", stones.len());
 }
 
-fn file_as_vec2(path: &str) -> Vec<Vec<char>> {
+fn evolve_stones(stones: Vec<i64>) -> Vec<i64> {
+    let mut result = Vec::new();
+    for stone in stones {
+        let digits = format!("{}", stone);
+        let digits_len = digits.len();
+        if stone == 0 {
+            result.push(1);
+        } else if digits_len % 2 == 0 {
+            let top = digits[0..digits_len/2].parse::<i64>().unwrap();
+            let bottom = digits[digits_len/2..].parse::<i64>().unwrap();
+            result.push(top);
+            result.push(bottom);
+        } else {
+            result.push(stone * 2024);
+        }
+    }
+    return result;
+}
+
+fn file_as_vec(path: &str) -> Vec<i64> {
     let mut result = Vec::new();
     let contents = file_contents(path);
-    let rows = contents.lines();
-    for row in rows {
-        let mut row_vec:Vec<char> = Vec::new();
-        let values = row.chars();
-        for value in values {
-            row_vec.push(value);
-        }
-        result.push(row_vec);
+    let strings = contents.trim().split_whitespace();
+    for s in strings {
+        result.push(s.parse::<i64>().expect("parse error"));
     }
     return result;
 }
@@ -47,10 +75,4 @@ fn file_contents(path: &str) -> String {
     use std::fs;
     return fs::read_to_string(path)
         .expect("should have read the file");
-}
-
-fn pretty_print(vec:&Vec<Vec<char>>) {
-    for row in vec {
-        println!("{:?}, ", row);
-    }
 }
