52788cda8840bbcfbf90466b6e58c70e16b1f95e
Mark Bessey <mark_bessey@yahoo.com>
2024-12-13 23:08:55 -0800

    Sample data works.

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,11 +1,34 @@
 use std::{collections::HashMap};
+use regex::Regex;
 
 #[derive(Debug)]
 struct Coord {
-    row: i32,
-    col: i32
+    x: i32,
+    y: i32
 }
 
+#[derive(Debug)]
+struct Game {
+    a: Coord,
+    b: Coord,
+    prize: Coord,
+}
+
+#[derive(Debug)]
+struct Solution {
+    a_presses: i32,
+    b_presses: i32,
+}
+
+impl Game {
+    fn new() -> Game {
+        Game{
+            a: Coord { x: 0, y: 0 },
+            b: Coord { x: 0, y: 0 },
+            prize: Coord { x: 0, y: 0 },
+        }
+    }
+}
 fn main() {
     sample();
     part1();
@@ -14,8 +37,24 @@ fn main() {
 
 fn sample() {
     println!("Sample: ");
-    let values = file_as_vec2("src/sample.txt");
-    //pretty_print(&values);
+    let games = file_as_games("src/sample.txt");
+    let mut total = 0;
+    for game in games {
+        // println!("{:?}", game);
+        let solutions = solve_game(game);
+        // println!("Solutions: {:?}", solutions);
+        let lowest = lowest_cost(solutions);
+        match lowest {
+            Some(x) => {
+                println!("Lowest cost: {}", x);
+                total += x;
+            },
+            None => {
+                println!("No solution");
+            }
+        }
+    }
+    println!("Total Cost: {}", total);
 }
 
 fn part1() {
@@ -28,17 +67,61 @@ fn part2() {
 
 }
 
-fn file_as_vec2(path: &str) -> Vec<Vec<char>> {
+fn solve_game(game: Game) -> Vec<Solution> {
+    let mut result = Vec::new();
+    for a in 0..100 {
+        for b in 0..100 {
+            let x = a * game.a.x + b * game.b.x;
+            let y = a * game.a.y + b * game.b.y;
+            if x == game.prize.x && y == game.prize.y {
+                result.push(Solution { a_presses: a, b_presses: b });
+            }
+        }
+    }
+    return result;
+}
+
+fn lowest_cost(solutions: Vec<Solution>) -> Option<i32> {
+    if solutions.len() == 0 {
+        return None
+    }
+    let mut lowest = cost(&solutions[0]);
+    for solution in solutions {
+        let cost = cost(&solution);
+        if cost < lowest { lowest = cost }
+    }
+    return Some(lowest)
+}
+
+fn cost(solution: &Solution) -> i32 {
+    solution.a_presses * 3 + solution.b_presses
+}
+
+fn file_as_games(path: &str) -> Vec<Game> {
     let mut result = Vec::new();
     let contents = file_contents(path);
-    let rows = contents.lines();
-    for row in rows {
-        let mut row_vec:Vec<char> = Vec::new();
-        let values = row.chars();
-        for value in values {
-            row_vec.push(value);
+    let line_re = Regex::new(r"(Button A|Button B|Prize): X[+=]([0-9]+), Y[+=]([0-9]+)")
+    .expect("Regex error");
+    let mut game = Game::new();
+    for capture in line_re.captures_iter(&contents) {
+        let x = capture[2].parse::<i32>()
+        .expect("can't parse X");
+        let y = capture[3].parse::<i32>()
+        .expect("can't parse X");
+        match &capture[1] {
+            "Button A" => {
+                game.a = Coord{x: x, y: y};
+            },
+            "Button B" => {
+                game.b = Coord{x: x, y: y};
+            },
+            "Prize" => {
+                game.prize = Coord{x: x, y: y};
+                result.push(game);
+                game = Game::new();
+            },
+            _ => ()
         }
-        result.push(row_vec);
     }
     return result;
 }
@@ -47,10 +130,4 @@ fn file_contents(path: &str) -> String {
     use std::fs;
     return fs::read_to_string(path)
         .expect("should have read the file");
-}
-
-fn pretty_print(vec:&Vec<Vec<char>>) {
-    for row in vec {
-        println!("{:?}, ", row);
-    }
 }
