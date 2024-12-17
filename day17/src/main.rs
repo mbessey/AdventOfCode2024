f8a0cfe863213471c88d81e448e1d90080a47e36
Mark Bessey <mark_bessey@yahoo.com>
2024-12-17 13:46:54 -0800

    Removed allocations, still too slow

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,5 +1,3 @@
-use std::cmp::min;
-
 use regex::Regex;
 
 type Number = i64;
@@ -12,7 +10,137 @@ struct Program {
     halted: bool,
     instructions: Vec<Number>,
     output: Vec<Number>,
-    check_output: bool
+    check_output: bool,
+    initial_a: Number,
+    initial_b: Number,
+    initial_c: Number,
+}
+
+impl Program {
+    fn new(a: Number, b: Number, c: Number, instructions: Vec<Number>) -> Program {
+        let len = instructions.len();
+        Program { 
+            reg_a: a,
+            reg_b: b,
+            reg_c: c, 
+            ip: 0,
+            halted: false,
+            instructions: instructions,
+            output: Vec::with_capacity(len),
+            check_output: false,
+            initial_a: a,
+            initial_b: b,
+            initial_c: c,
+        }
+    }
+
+    fn reset(self: &mut Program) {
+        self.reg_a = self.initial_a;
+        self.reg_b = self.initial_b;
+        self.reg_c = self.initial_c;
+        self.ip = 0;
+        self.halted = false;
+        self.output.clear();
+    }
+
+    fn step(self: &mut Program) {
+        let instruction = self.instructions[self.ip];
+        self.ip += 1;
+        match instruction {
+            0 => { self.adv() }, // divide A / 2^combo => A
+            1 => { self.bxl() }, // bitwise xor literal B|L => B
+            2 => { self.bst() }, // mod 8 combo%8 => B
+            3 => { self.jnz() }, // jump if non-zero
+            4 => { self.bxc() }, // bitwise XOR b|c => B
+            5 => { self.out() }, // output
+            6 => { self.bdv() }, // divide A / 2^combo => B
+            7 => { self.cdv() }, // divide A / 2^combo => C
+            _ => {
+                panic!("bad instruction: {}", instruction)
+            }
+        }
+        if self.ip >= self.instructions.len() {
+            self.halted = true;
+        }
+    }
+    
+    fn adv(self: &mut Program) {
+        let c = self.combo_operand();
+        let result = self.reg_a / (1 << c);
+        self.reg_a = result;
+    }
+    
+    fn bxl(self: &mut Program) {
+        let l = self.literal_operand();
+        self.reg_b = self.reg_b ^ l;
+    }
+    
+    fn bst(self: &mut Program) {
+        let c = self.combo_operand();
+        self.reg_b = c % 8;
+    }
+    
+    fn jnz(self: &mut Program) {
+        let target = self.literal_operand();
+        if self.reg_a != 0 {
+            self.ip = target as usize
+        }
+    }
+    
+    fn bxc(self: &mut Program) {
+        let _ = self.literal_operand();
+        let b = self.reg_b;
+        let c = self.reg_c;
+        self.reg_b = b ^ c;
+    }
+    
+    fn out(self: &mut Program) {
+        let c = self.combo_operand();
+        let v = c % 8;
+        self.output.push(v);
+        if self.check_output {
+            if v != self.instructions[self.output.len()-1] {
+                self.halted = true;
+            }
+        }
+    }
+    
+    fn bdv(self: &mut Program) {
+        let c = self.combo_operand();
+        let result = self.reg_a / (1 << c);
+        self.reg_b = result;
+    }
+    
+    fn cdv(self: &mut Program) {
+        let c = self.combo_operand();
+        let result = self.reg_a / (1 << c);
+        self.reg_c = result;
+    }
+    
+    fn literal_operand(self: &mut Program) -> Number {
+        let operand = self.instructions[self.ip];
+        self.ip += 1;
+        return operand
+    }
+    
+    fn combo_operand(self: &mut Program) -> Number {
+        let operand = self.instructions[self.ip];
+        self.ip += 1;
+        match operand {
+            0..=3 => operand,
+            4 => self.reg_a,
+            5 => self.reg_b,
+            6 => self.reg_c,
+            _ => panic!("invalid combo operand")
+        }
+    }
+    
+    fn run(self: &mut Program) {
+        while ! self.halted {
+            self.step();
+            // println!("Program state: {:?}", program_state);
+        }
+    }
 }
 
 fn main() {
@@ -26,7 +154,7 @@ fn sample() {
     let mut program_state = file_as_program("src/sample.txt");
     println!("Program state: {:?}", program_state);
     while ! program_state.halted {
-        step(&mut program_state);
+        program_state.step();
         println!("Program state: {:?}", program_state);
     }
     println!("Halted.");
@@ -36,7 +164,7 @@ fn part1() {
     println!("PART ONE: ");
     let mut program_state = file_as_program("src/part1.txt");
     println!("Program state: {:?}", program_state);
-    run(&mut program_state);
+    program_state.run();
     println!("Halted.");
     let output_strings:Vec<String> = program_state.output.iter().map(|x|format!("{}", x)).collect();
     println!("{}", output_strings.join(","));
@@ -44,120 +172,21 @@ fn part1() {
 
 fn part2() {
     println!("PART TWO: ");
-    let program_state = file_as_program("src/part1.txt");
-    let mut start_a = 2381000000;
+    let mut program_state = file_as_program("src/part1.txt");
+    let mut start_a = 10473000000;
+    program_state.check_output = true;
     loop {
-        let mut state = program_state.clone();
-        state.check_output = true;
-        state.reg_a = start_a;
+        program_state.reg_a = start_a;
         if start_a % 1000000 == 0 {
-            println!("reg_a == {}", state.reg_a);
+            println!("reg_a == {}", program_state.reg_a);
         }
-        run(&mut state);
-        if state.output == state.instructions {
+        program_state.run();
+        if program_state.output == program_state.instructions {
             println!("Output matched with a = {}", start_a);
             break;
         }
         start_a += 1;
-    }
-}
-
-fn run(program: &mut Program) {
-    while ! program.halted {
-        step(program);
-        // println!("Program state: {:?}", program_state);
-    }
-}
-
-fn step(program: &mut Program) {
-    let instruction = program.instructions[program.ip];
-    program.ip += 1;
-    match instruction {
-        0 => { adv(program) }, // divide A / 2^combo => A
-        1 => { bxl(program) }, // bitwise xor literal B|L => B
-        2 => { bst(program) }, // mod 8 combo%8 => B
-        3 => { jnz(program) }, // jump if non-zero
-        4 => { bxc(program) }, // bitwise XOR b|c => B
-        5 => { out(program) }, // output
-        6 => { bdv(program) }, // divide A / 2^combo => B
-        7 => { cdv(program) }, // divide A / 2^combo => C
-        _ => {
-            panic!("bad instruction: {}", instruction)
-        }
-    }
-    if program.ip >= program.instructions.len() {
-        program.halted = true;
-    }
-}
-
-fn adv(prog: &mut Program) {
-    let c = combo_operand(prog);
-    let result = prog.reg_a / (1 << c);
-    prog.reg_a = result;
-}
-
-fn bxl(prog: &mut Program) {
-    let l = literal_operand(prog);
-    prog.reg_b = prog.reg_b ^ l;
-}
-
-fn bst(prog: &mut Program) {
-    let c = combo_operand(prog);
-    prog.reg_b = c % 8;
-}
-
-fn jnz(prog: &mut Program) {
-    let target = literal_operand(prog);
-    if prog.reg_a != 0 {
-        prog.ip = target as usize
-    }
-}
-
-fn bxc(prog: &mut Program) {
-    let _ = literal_operand(prog);
-    let b = prog.reg_b;
-    let c = prog.reg_c;
-    prog.reg_b = b ^ c;
-}
-
-fn out(prog: &mut Program) {
-    let c = combo_operand(prog);
-    let v = c % 8;
-    prog.output.push(v);
-    if prog.check_output {
-        if v != prog.instructions[prog.output.len()-1] {
-            prog.halted = true;
-        }
-    }
-}
-
-fn bdv(prog: &mut Program) {
-    let c = combo_operand(prog);
-    let result = prog.reg_a / (1 << c);
-    prog.reg_b = result;
-}
-
-fn cdv(prog: &mut Program) {
-    let c = combo_operand(prog);
-    let result = prog.reg_a / (1 << c);
-    prog.reg_c = result;
-}
-
-fn literal_operand(program: &mut Program) -> Number {
-    let operand = program.instructions[program.ip];
-    program.ip += 1;
-    return operand
-}
-
-fn combo_operand(program: &mut Program) -> Number {
-    let operand = program.instructions[program.ip];
-    program.ip += 1;
-    match operand {
-        0..=3 => operand,
-        4 => program.reg_a,
-        5 => program.reg_b,
-        6 => program.reg_c,
-        _ => panic!("invalid combo operand")
+        program_state.reset();
     }
 }
 
@@ -167,36 +196,31 @@ fn file_as_program(path: &str) -> Program {
     .expect("Regex error");
     let program_re = Regex::new(r"Program: ([0-9]+(,[0-9]+)*)")
     .expect("Regex error");
-    let mut prog = Program {
-        reg_a: 0,
-        reg_b: 0,
-        reg_c: 0,
-        ip: 0,
-        halted: false,
-        instructions: Vec::new(),
-        output: Vec::new(),
-        check_output: false
-    };
+    let mut a =0;
+    let mut b= 0;
+    let mut c =0;
+    let mut i = vec![];
+    
     for line in contents.lines() {
         if let Some(register) = reg_re.captures(line) {
             // println!("reg: {:?}", register);
             let value = register[2].parse::<Number>()
             .expect("parse error");
             match &register[1] {
-                "Register A" => prog.reg_a = value,
-                "Register B" => prog.reg_b = value,
-                "Register C" => prog.reg_c = value,
+                "Register A" => a = value,
+                "Register B" => b = value,
+                "Register C" => c = value,
                 _ => { panic!() }
             }
         } else if let Some(program) = program_re.captures(line) {
             // println!("prog: {:?}", program);
             let instructions:Vec<&str> = program[1].split(",").collect();
-            prog.instructions = instructions.into_iter().map(|s| s.parse::<Number>().unwrap()).collect();
+            i = instructions.into_iter().map(|s| s.parse::<Number>().unwrap()).collect();
         } else {
-            println!("Ignoring line: {}", line);
+            //println!("Ignoring line: {}", line);
         }
     }
-    return prog;
+    return Program::new(a, b, c, i);
 }
 
 fn file_contents(path: &str) -> String {
