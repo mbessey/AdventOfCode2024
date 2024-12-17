ebb0dbe6364b6f40cca2fdbac7e0707d3bb0f5d8
Mark Bessey <mark_bessey@yahoo.com>
2024-12-16 22:09:00 -0800

    Day 9, part 1 done

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,8 @@
+use std::path::MAIN_SEPARATOR;
+
+
+type Number = i64;
+type Map = Vec<Option<Number>>;
 
 fn main() {
     sample();
@@ -7,14 +12,22 @@ fn main() {
 
 fn sample() {
     println!("Sample: ");
-    let values = file_as_vec("src/sample.txt");
-    // pretty_print(&values);
-
+    let mut map = file_as_map("src/sample.txt");
+    println!("{:?}", &map);
+    pretty_print(&map);
+    compact(&mut map);
+    pretty_print(&map);
+    println!("Checksum: {}", checksum(&map));
 }
 
 fn part1() {
     println!("PART ONE: ");
-
+    let mut map = file_as_map("src/part1.txt");
+    println!("Map length: {}", map.len());
+    // pretty_print(&map);
+    compact(&mut map);
+    // pretty_print(&map);
+    println!("Checksum: {}", checksum(&map));
 }
 
 fn part2() {
@@ -22,9 +35,55 @@ fn part2() {
 
 }
 
-fn file_as_vec(path: &str) -> Vec<char> {
+fn checksum(map: &Map) -> Number {
+    let mut cksum = 0;
+    for block in 0..map.len() {
+        match map[block] {
+            None => (),
+            Some(x) => { cksum += block as Number * x as Number }
+        }
+    }
+    return cksum;
+}
+
+fn compact(map: &mut Map) {
+    let mut left = 0;
+    let mut right = map.len() - 1;
+    while right > left {
+        if map[left] == None && map[right] != None {
+            let temp = map[right];
+            map[right] = map[left];
+            map[left] = temp;
+        }
+        if map[right] == None {
+            right -= 1;
+        }
+        if map[left] != None {
+            left += 1;
+        }
+    }
+}
+
+fn file_as_map(path: &str) -> Map {
+    let mut map = Map::new();
     let contents = file_contents(path);
-    return contents.trim().chars().collect();
+    let mut is_file = true;
+    let mut file_id = 0;
+    for digit in contents.trim().chars() {
+        let length = digit as Number - '0' as Number;
+        let value: Option<Number>;
+        if is_file {
+            value = Some(file_id);
+            file_id += 1;
+        } else {
+            value = None
+        }
+        for _i in 0..length {
+            map.push(value);
+        }
+        is_file = !is_file;
+    }
+    return map;
 }
 
 fn file_contents(path: &str) -> String {
@@ -33,6 +92,12 @@ fn file_contents(path: &str) -> String {
         .expect("should have read the file");
 }
 
-fn pretty_print(vec:&Vec<char>) {
-    println!("{:?}, ", vec);
+fn pretty_print(map: &Map) {
+    for block in map {
+        match block {
+            Some(x) => print!("{}", x),
+            None => print!(".")
+        }
+    }
+    println!("")
 }
